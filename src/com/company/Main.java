93fdfdc4ebfea46bc8b8e3018d2622c94a155a77
Kulachenkov <nik.k2710@gmail.com>
2021-11-24 21:05:44 +0300

    1st task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        //taskOne();
 
 
     }
 
     public static void taskOne(){
+
+        // Задание 1
+
+        int count = 0;
+        while (count <10){
+            count++;
+            System.out.print(count + " ");
+
+        }
+        System.out.println(" ");
+
+        for (int i = 10; i > 0; i--){
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void taskTwo(){
+
 
     }
 }
