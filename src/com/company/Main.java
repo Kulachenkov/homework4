8e4f8ce910e70e92533518756d7764d9be7d9690
Kulachenkov <nik.k2710@gmail.com>
2021-11-24 21:40:40 +0300

    finally!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         //taskOne();
+        //taskTwo();
+        taskThree();
 
 
     }
@@ -26,6 +28,37 @@ public class Main {
     }
 
     public static void taskTwo(){
+        // Задание 2
+
+        int dayOfFriday = 5;
+
+        for (int i = 1; i <= 31; i++){
+            if ((i-dayOfFriday) % 7 == 0){
+                System.out.println("Сегодня пятница, " + i + "-е число");
+            }
+        }
+
+    }
+
+    public static void taskThree(){
+        // Задание 3
+
+        int currentYear = 2021;
+        int previousYear = 2021;
+        int nextYear = 2021;
+
+        while((currentYear - previousYear) <= 200){
+            previousYear--;
+            if (previousYear % 79 == 0){
+                System.out.println(previousYear);
+            }
+        }
+        while ((nextYear - currentYear) <= 100){
+            nextYear++;
+            if(nextYear % 79 == 0){
+                System.out.println(nextYear);
+            }
+        }
 
 
     }
